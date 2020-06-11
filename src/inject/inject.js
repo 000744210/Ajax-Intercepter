dd865683e247cb0d548ff77768a6ce9bfb48d1c5
supergoldstars <supergoldstars1@gmail.com>
2020-06-11 19:48:26 -0400

    added library loader

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -13,62 +13,62 @@
             docs.push(document);
 
             docs.forEach(function(doc){
-            	
-	            chrome.storage.local.get(["ajaxGroupData" + id], function (result) {
-	                var urls = result['ajaxGroupData' + id] || {};
-	                s = doc.createElement('script');
-	                s.innerHTML = "var _FILTER_URLS = " + JSON.stringify(urls);
+                
+                chrome.storage.local.get(["ajaxGroupData" + id], function (result) {
+                    var urls = result['ajaxGroupData' + id] || {};
+                    s = doc.createElement('script');
+                    s.innerHTML = "var _FILTER_URLS = " + JSON.stringify(urls);
 
-	                (doc.head || doc.documentElement).appendChild(s);
-	            })
+                    (doc.head || doc.documentElement).appendChild(s);
+                })
 
-	            s = doc.createElement('script');
-	            s.src = chrome.runtime.getURL('/js/sweetalert2.js');
-	            s.onload = function () {
-	                this.remove();
-	            };
-	            (doc.head || doc.documentElement).appendChild(s);
+                s = doc.createElement('script');
+                s.src = chrome.runtime.getURL('/js/sweetalert2.js');
+                s.onload = function () {
+                    this.remove();
+                };
+                (doc.head || doc.documentElement).appendChild(s);
 
-	            s = doc.createElement('script');
-	            s.src = chrome.runtime.getURL('src/inject/injected.js');
-	            s.onload = function () {
-	                this.remove();
-	            };
-	            (doc.head || doc.documentElement).appendChild(s);
+                s = doc.createElement('script');
+                s.src = chrome.runtime.getURL('src/inject/injected.js');
+                s.onload = function () {
+                    this.remove();
+                };
+                (doc.head || doc.documentElement).appendChild(s);
 
-	            var logger = doc.createElement("logger")
-	            doc.documentElement.appendChild(logger);
+                var logger = doc.createElement("logger")
+                doc.documentElement.appendChild(logger);
 
-	            // Listens for when a new api object gets inserted into the logger object.
-	            // This would have been better as a onunload event but due to how async code can't be ran in there we had to do this.
-	            logger.addEventListener( 'DOMNodeInserted',function(e){
+                // Listens for when a new api object gets inserted into the logger object.
+                // This would have been better as a onunload event but due to how async code can't be ran in there we had to do this.
+                logger.addEventListener( 'DOMNodeInserted',function(e){
 
-	                var apis = doc.querySelectorAll("html > logger > api");
-	                var apiUrls = []
-	                apis.forEach(api => apiUrls.push({url: api.dataset.url,params:api.dataset.params.split(",")}))
-	            
-	                
-	                chrome.storage.local.get(["ajaxLoggerData" + id], function (result) {
-	                    var loggedAPIs = result['ajaxLoggerData' + id] || {};
-	                    
-	                    //var apiSet = new Set(loggedAPIs);
-	                    apiUrls.forEach(function(api){ 
-	                        loggedAPIs[api.url] = new Set(loggedAPIs[api.url])
-	                        api.params.forEach(function(param){
-	                            if(param.length > 0){
-	                                loggedAPIs[api.url].add(param)
-	                            }
-	                        });
-	                        loggedAPIs[api.url] = [...loggedAPIs[api.url]]
-	                    });
-	                    
-	                    
-	                    var jsonObj = {}
-	                    jsonObj["ajaxLoggerData" + id] = loggedAPIs;
-	                    
-	                    chrome.storage.local.set(jsonObj);
-	                })                
-	            });
+                    var apis = doc.querySelectorAll("html > logger > api");
+                    var apiUrls = []
+                    apis.forEach(api => apiUrls.push({url: api.dataset.url,params:api.dataset.params.split(",")}))
+                
+                    
+                    chrome.storage.local.get(["ajaxLoggerData" + id], function (result) {
+                        var loggedAPIs = result['ajaxLoggerData' + id] || {};
+                        
+                        //var apiSet = new Set(loggedAPIs);
+                        apiUrls.forEach(function(api){ 
+                            loggedAPIs[api.url] = new Set(loggedAPIs[api.url])
+                            api.params.forEach(function(param){
+                                if(param.length > 0){
+                                    loggedAPIs[api.url].add(param)
+                                }
+                            });
+                            loggedAPIs[api.url] = [...loggedAPIs[api.url]]
+                        });
+                        
+                        
+                        var jsonObj = {}
+                        jsonObj["ajaxLoggerData" + id] = loggedAPIs;
+                        
+                        chrome.storage.local.set(jsonObj);
+                    })                
+                });
             })
         }else{
             console.log(window.location.hostname + " does not end with " + url)
@@ -92,6 +92,27 @@ chrome.storage.local.get(["domainGroupData"], function (result) {
                 s.innerHTML = "var _FILTER_URLS = " + JSON.stringify(urls);
 
                 (document.head || document.documentElement).appendChild(s);
+                
+                var libSet = new Set();
+                for(let groupId in urls){
+                    urls[groupId].libs.split("\n").forEach(function(lib){
+                        lib = lib.trim();
+                        libSet.add(lib);
+                    })
+                    
+                }
+                console.log(libSet)
+                libSet.forEach(function(libUrl){
+                    var script = document.createElement('script');
+                    script.setAttribute("type", "text/javascript");
+                    script.onload = function () {
+                        this.remove();
+                    };
+                    script.src = libUrl;
+                    console.log("loaded",script);
+                    (document.head || document.documentElement).appendChild(script);
+                })
+                
             })
             
             s = document.createElement('script');
--- src/page_action/createajaxurl.js
+++ src/page_action/createajaxurl.js
@@ -1,9 +1,9 @@
 function getQueryParameter(parameter) {
-	try{
-		return (window.location.href.split(parameter + '=')[1].split('&')[0]);
-	}catch(e){
-		return null;
-	}
+    try{
+        return (window.location.href.split(parameter + '=')[1].split('&')[0]);
+    }catch(e){
+        return null;
+    }
 }
 
 
@@ -13,44 +13,45 @@ var displayUrl = getQueryParameter("displayurl")
 document.getElementById("editGroupUrl").href = "/src/page_action/editgroup.html?id="+groupId
 
 if(updateId!=null){
-	chrome.storage.local.get(["ajaxGroupData"+groupId], function(result) {
+    chrome.storage.local.get(["ajaxGroupData"+groupId], function(result) {
         var array = result["ajaxGroupData"+groupId]?result["ajaxGroupData"+groupId]:{};
-		document.getElementById("domain").value = array[updateId].url;
-		document.getElementById("automation").checked = array[updateId].isAutomated;
-		document.getElementById("codeBox").value = array[updateId].code;
-		
-		updateCodeBox()
-	})
+        document.getElementById("domain").value = array[updateId].url;
+        document.getElementById("automation").checked = array[updateId].isAutomated;
+        document.getElementById("codeBox").value = array[updateId].code;
+        document.getElementById("libBox").value = array[updateId].libs;
+        
+        updateCodeBox()
+    })
 }else if(displayUrl){
-	document.getElementById("domain").value = displayUrl;
+    document.getElementById("domain").value = displayUrl;
 }
 
 document.getElementById("continueBtn").onclick = function(){
-	var domainRegex = /^(?:([A-Za-z\*]+):)?(\/{0,3})([0-9.\-A-Za-z\*]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/
-	
-	var url = document.getElementById("domain").value
-	
-	if(!url.match(domainRegex)){
-		alert("Ajax url must be valid. (https://api.google.com/getinfo)")
-		return;
-	}
-	
-	chrome.storage.local.get(["ajaxGroupData"+groupId], function(result) {
+    var domainRegex = /^(?:([A-Za-z\*]+):)?(\/{0,3})([0-9.\-A-Za-z\*]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/
+    
+    var url = document.getElementById("domain").value
+    
+    if(!url.match(domainRegex)){
+        alert("Ajax url must be valid. (https://api.google.com/getinfo)")
+        return;
+    }
+    
+    chrome.storage.local.get(["ajaxGroupData"+groupId], function(result) {
         var array = result["ajaxGroupData"+groupId]?result["ajaxGroupData"+groupId]:{};
 
-		var nextId;
-		
+        var nextId;
 
-		if(updateId!=null){
-			nextId = parseInt(updateId)
-		}else{
-			nextId = parseInt(getLargestKey(array))+1
-		}
-		array[nextId] = {
-			url:document.getElementById("domain").value,
-			isAutomated: document.getElementById("automation").checked,
-			code: document.getElementById("codeBox").value
-		};
+        if(updateId!=null){
+            nextId = parseInt(updateId)
+        }else{
+            nextId = parseInt(getLargestKey(array))+1
+        }
+        array[nextId] = {
+            url:document.getElementById("domain").value,
+            isAutomated: document.getElementById("automation").checked,
+            code: document.getElementById("codeBox").value,
+            libs:document.getElementById("libBox").value
+        };
         var jsonObj = {};
 
         jsonObj["ajaxGroupData"+groupId] = array;
@@ -58,28 +59,28 @@ document.getElementById("continueBtn").onclick = function(){
             window.location.href="/src/page_action/editgroup.html?id="+groupId;
         });
     });
-	debugger;
+    debugger;
 }
 
 function updateCodeBox()
 {
     if($("#automation").is(':checked')) {
-        $("#codeBox").css("display","block")
+        $("#automationSection").css("display","block")
     }else{
-		$("#codeBox").css("display","none")
-		
-	}
+        $("#automationSection").css("display","none")
+        
+    }
 }
 
 $("#automation").change(updateCodeBox);
 
 function getLargestKey(obj){
-	var keys = Object.keys(obj)
-	largest = 0;
-	for(i=0;i<keys.length;i++){
-		if(keys[i] > largest){
-			largest = keys[i]
-		}
-	}
-	return largest;
+    var keys = Object.keys(obj)
+    largest = 0;
+    for(i=0;i<keys.length;i++){
+        if(keys[i] > largest){
+            largest = keys[i]
+        }
+    }
+    return largest;
 }
